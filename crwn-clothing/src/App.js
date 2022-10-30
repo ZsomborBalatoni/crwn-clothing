1a5b7782959e1063b9436146efa58904b0998757
ZsomborBalatoni <zsombor.balatoni@gmail.com>
2022-10-30 16:11:38 +0100

    Implementing Sign-in, installing firebase, etc

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component.jsx';
 
 const Shop = () => {
   return <h1>I am the shop page</h1>
@@ -14,6 +15,7 @@ const App = () => {
       <Route path ='/' element={<Navigation />}>
         <Route index={true} element={<Home />} />
         <Route path='shop' index element={<Shop />} />
+        <Route path='sign-in' index element={<SignIn />} />
       </Route>
     </Routes>
   );
